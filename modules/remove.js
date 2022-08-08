1f042b0864c8cc41ad02a481715f4d93d1cb54da
addis <betsega23@gmail.com>
2022-08-09 02:01:48 +0300

    Fix linter error

--- modules/remove.js
+++
@@ -1,8 +0,0 @@
-import { populateStorage } from './populate.js';
-//import { collection } from './Collection.js';
-const removeFromColl = (data) => {
-  //const coll = new collection();
-  const arr = data.getAttribute('data-value').split('-');
-  books = books.filter((item) => item.title + item.author !== arr[0] + arr[1]);
-  populateStorage();
-};
